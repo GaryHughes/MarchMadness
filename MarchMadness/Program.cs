5104667a45d48c11bbeec20b361d0b3a39550e74
Benjamin Smith <ben.smith3@gmail.com>
2021-01-26 09:00:51 +1100

    Catch exceptions and fix program arguments.

--- MarchMadness/Program.cs
+++ MarchMadness/Program.cs
@@ -4,37 +4,47 @@ namespace MarchMadness
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            var filename = (args.Length > 1) ? args[1] : "Teams.csv";
-
-            var bracket = new Bracket();
-            bracket.LoadTeams(filename);
-
-            while (!bracket.IsComplete())
+            try
             {
-                bracket.CreateNextRound();
-                Console.WriteLine($"Round #{bracket.RoundNumber}");
-                Console.WriteLine("--------");
+                var filename = (args.Length > 0) ? args[0] : "Teams.csv";
 
-                var lastRound = bracket.LastRound ?? throw new Exception("No last round defined.");
-                foreach (var game in bracket.LastRound.Games)
+                var bracket = new Bracket();
+                bracket.LoadTeams(filename);
+
+                while (!bracket.IsComplete())
                 {
-                    game.Resolve(ChooseWinner(game));
-                    Console.WriteLine($"\t({game.Winner?.Name})");
+                    bracket.CreateNextRound();
+                    Console.WriteLine($"Round #{bracket.RoundNumber}");
+                    Console.WriteLine("--------");
+
+                    var lastRound = bracket.LastRound ?? throw new Exception("No last round defined.");
+                    foreach (var game in bracket.LastRound.Games)
+                    {
+                        game.Resolve(ChooseWinner(game));
+                        Console.WriteLine($"\t({game.Winner?.Name})");
+                    }
+
+                    Console.WriteLine();
                 }
 
+                var champion = bracket.FindChampion() ?? throw new Exception("No champion found");
+                Console.WriteLine($"Champion is: {champion.Name}");
                 Console.WriteLine();
+
+                Console.WriteLine("Path to Victory: ");
+                foreach (var game in bracket.ListGames(champion))
+                {
+                    Console.WriteLine($"  Round {game.RoundNumber}: defeated {game.Loser?.Name}");
+                }
+
+                return 0;
             }
-
-            var champion = bracket.FindChampion() ?? throw new Exception("No champion found");
-            Console.WriteLine($"Champion is: {champion.Name}");
-            Console.WriteLine();
-
-            Console.WriteLine("Path to Victory: ");
-            foreach (var game in bracket.ListGames(champion))
+            catch (Exception exception)
             {
-                Console.WriteLine($"  Round {game.RoundNumber}: defeated {game.Loser?.Name}");
+                Console.WriteLine($"ERROR: {exception.Message}");
+                return 1;
             }
         }
 
