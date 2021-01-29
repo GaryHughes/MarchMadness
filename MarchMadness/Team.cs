1750afaea96722073e98260e559e09313808e60b
Gary Hughes <garyedwardhughes@gmail.com>
2021-01-29 18:06:36 +1100

    Simplify record.

--- MarchMadness/Team.cs
+++ MarchMadness/Team.cs
@@ -2,17 +2,5 @@ using System;
 
 namespace MarchMadness
 {
-    public record Team
-    {
-        public Team(string name, int seed, string region)
-        {
-            Name = name;
-            Seed = seed;
-            Region = region;
-        }
-
-        public string Name { get; }
-        public int Seed { get; }
-        public string Region { get; }
-    }
+    public record Team(string Name, int Seed, string Region);
 }
